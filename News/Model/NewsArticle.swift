59aee0abe1cadb71b3c387529f69500f6a2899a2
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-13 22:23:56 -0800

    Set Up NewsArticleModel

---
+++ News/Model/NewsArticle.swift
@@ -0,0 +1,25 @@
+//
+//  NewsArticle.swift
+//  News
+//
+//  Created by Kabir Dhillon on 2/13/23.
+//
+
+import Foundation
+
+struct NewsArticle: Decodable {
+    
+    struct Source: Decodable {
+        var id: String?
+        var name: String
+    }
+    
+    var author: String?
+    var title: String
+    var description: String?
+    var url: String
+    var urlToImage: String
+    var publishedAt: String
+    var content: String?
+    
+}
