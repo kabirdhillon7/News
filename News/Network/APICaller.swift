f1c888ffd96730590ee6d624770ed79902b45d0b
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-13 22:25:10 -0800

    Added APICaller
    To handle networking call to News API

---
+++ News/Network/APICaller.swift
@@ -0,0 +1,30 @@
+//
+//  APICaller.swift
+//  News
+//
+//  Created by Kabir Dhillon on 2/13/23.
+//
+
+import Foundation
+import Combine
+
+enum API: String {
+    case key = "bdbf20fb6b914a6ca553b79c45c5226a"
+}
+
+class APICaller {
+    
+    // Top Headline
+    func getTopHeadline() -> AnyPublisher<[NewsArticle], Error> {
+        guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=\(API.key.rawValue)") else {
+            return Fail(error: NSError(domain: "Invalid url", code: 0)).eraseToAnyPublisher()
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        return URLSession.shared.dataTaskPublisher(for: urlRequest)
+            .map { $0.data }
+            .decode(type: [NewsArticle].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
